4ae68b5387ad731bfc14b034ec3290ee7f9d2d46
clairvy <clairvy@gmail.com>
2014-08-30 03:26:38 +0100

    append task to symlink environment

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -13,3 +13,13 @@ set :use_sudo, false
 set :use_composer, true
 set :ssh_options, :keys => "/home/kitchen/.ssh/app1/id_rsa"
 logger.level = Logger::MAX_LEVEL
+
+# other task
+after "deploy:share_childs", "deploy:link_environment"
+namespace(:deploy) do
+  task :link_environment do
+    run <<-CMD
+      cd #{File.join(release_path, %w|app|)} && ln -nfs environment_staging.php environment.php
+    CMD
+  end
+end
