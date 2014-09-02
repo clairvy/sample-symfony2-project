ce2ec6d52b28a2aa681508c6f85eef5e2d9fb0de
clairvy <clairvy@gmail.com>
2014-09-02 16:58:15 +0900

    deploy tested

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,19 +1,15 @@
-set :application, "set your application name here"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
-set :app_path,    "app"
+# to
+set :application, "sample-symfony2-project"
+set :deploy_to, "/var/www/staging"
+role :app, "app1"
+set :keep_releases, 3
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
-set :scm,         :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+# from
+set :repository, "https://github.com/clairvy/#{application}.git"
+set :scm, :git
 
-set :model_manager, "doctrine"
-# Or: `propel`
-
-role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain, :primary => true       # This may be the same as your `Web` server
-
-set  :keep_releases,  3
-
-# Be more verbose by uncommenting the following line
-# logger.level = Logger::MAX_LEVEL
+# setting
+set :use_sudo, false
+set :use_composer, true
+set :ssh_options, :keys => "/home/kitchen/.ssh/app1/id_rsa"
+logger.level = Logger::MAX_LEVEL
